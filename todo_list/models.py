a28463ee4251cd5f5fbecca6c16f8556a9df6f41
Luan0AVila <avila.almeida.luan@gmail.com>
2025-11-10 12:58:23 -0300

    criando o models do todo

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+
+    class Status(models.TextChoices):
+        PENDENTE = 'Pendente'
+        FINALIZADO = 'Finalizado'
+
+    tarefa = models.CharField(max_length=65, verbose_name='Titulo da tarefa')
+    descrição = models.CharField(max_length=165, verbose_name='Descrição da tarefa')
+    status = models.CharField(choices=Status.choices, default=Status.PENDENTE)
+    
+    
+    def __str__(self):
+        """Retorna uma representação da stirng do modelo"""
+        return f"{self.descrição[:50]}..."
