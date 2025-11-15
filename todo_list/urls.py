24ff8637fb1a0dc5c672c8406335e6e200a3af3d
Luan0Avila <avila.almeida.luan@gmail.com>
2025-11-15 19:32:35 -0300

    criando a view de edição de tarefas

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -6,4 +6,5 @@ app_name = 'todo_list'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('create/', views.CreateTodoView.as_view(), name='create_todo'),
+    path('edit/<int:pk>/', views.UpdateTodoView.as_view(), name='edit_todo'),
 ]
--- todo_list/views/todos.py
+++ todo_list/views/todos.py
@@ -1,7 +1,15 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from ..forms import TodoForm
+from django.urls import reverse_lazy
+from ..models import Todo
 
 class CreateTodoView(CreateView):
     template_name = 'todo_list/pages/create_todo.html'
     form_class = TodoForm
     success_url = '/'
+
+class UpdateTodoView(UpdateView):
+    model = Todo
+    fields = ['tarefa', 'descrição', 'status']
+    template_name = 'todo_list/pages/update_todo.html'
+    success_url = reverse_lazy('todo_list:home')
