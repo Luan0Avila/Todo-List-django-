20466911c3a64f8ecc0ec5fba06e7a22f0e4a504
Luan0Avila <avila.almeida.luan@gmail.com>
2025-11-16 20:13:55 -0300

    fazendo com que apareça apenas as tarefas do usuário

--- todo_list/views/home.py
+++ todo_list/views/home.py
@@ -1,8 +1,14 @@
 from django.views.generic import ListView
 from ..models import Todo
-
+from django.http import Http404
 
 class HomeView(ListView):
     model = Todo
     template_name = 'todo_list/pages/home.html'
     context_object_name = 'todos'
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Todo.objects.filter(user=self.request.user).order_by('-id')
+        return super().get_queryset()
+    
