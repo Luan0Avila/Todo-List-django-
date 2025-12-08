aea613ad8493b16aee0b27f76b564b09a871b089
Luan0AVila <avila.almeida.luan@gmail.com>
2025-12-08 12:49:35 -0300

    criando a autenticação da api

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,7 +1,7 @@
 from . import views
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView,TokenVerifyView
 app_name = 'todo_list'
 
 todo_api_router = SimpleRouter()
@@ -18,6 +18,18 @@ urlpatterns = [
     path('delete/<int:pk>/', views.DeleteTodoView.as_view(), name='delete_todo'),
     path('toggle/<int:pk>/', views.ToggleStatusView.as_view(), name='toggle_status'),
     path('search/', views.TodoSearchView.as_view(), name='search_todos'),
+        path(
+        'api/token/', 
+        TokenObtainPairView.as_view(), 
+        name='token_obtain_pair'),
+    path(
+        'api/token/refresh/', 
+        TokenRefreshView.as_view(), 
+        name='token_refresh'),
+    path(
+        'api/token/verify/', 
+        TokenVerifyView.as_view(), 
+        name='token_verify'),
 
         #por ultimo sempre
     path('', include(todo_api_router.urls)),
--- todo_list/views/api.py
+++ todo_list/views/api.py
@@ -2,12 +2,13 @@ from rest_framework.viewsets import ModelViewSet
 from ..models import Todo
 from ..serializers import TodoListSerializer
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class TodoAPIVieSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoListSerializer
     http_method_names = ['get','head','options','patch','post','delete']
-
+    permission_classes = [IsAuthenticatedOrReadOnly,]
     def get_object(self):
         pk = self.kwargs.get('pk', '')
 
