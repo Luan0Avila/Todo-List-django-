1a7277e6afac1de9a6dae04325b3024d8644ed44
Luan0Avila <avila.almeida.luan@gmail.com>
2025-12-25 17:08:13 -0300

    adicionando outra dependencia que estava faltando no deploy

--- todo_list/models.py
+++ todo_list/models.py
@@ -15,7 +15,7 @@ class Todo(models.Model):
 
     tarefa = models.CharField(max_length=65, verbose_name='Titulo da tarefa')
     descrição = models.CharField(max_length=165, verbose_name='Descrição da tarefa')
-    status = models.CharField(choices=Status.choices, default=Status.PENDENTE)
+    status = models.CharField(max_length=20,choices=Status.choices, default=Status.PENDENTE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     categoria = models.ManyToManyField(Category, blank=True)
     
